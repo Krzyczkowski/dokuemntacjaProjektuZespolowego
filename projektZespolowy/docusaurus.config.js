4f5267305bc413dc08e3e0f6b6db7dd1869eca20
K4am <krzyczkowski.wiktor00@gmail.com>
2023-04-03 13:08:47 +0200

    small

--- projektZespolowy/docusaurus.config.js
+++ projektZespolowy/docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
       navbar: {
         title: 'Strona główna',
         logo: {
-          alt: 'My Site Logo',
+          alt: '',
           src: 'img/logo.svg',
         },
         items: [
